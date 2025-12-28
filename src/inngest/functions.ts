4e9f6c9c50efb929296b9678a5959738abf2e468
Aditi <lovethyneighbourasthyself55@gmail.com>
2025-12-28 10:49:08 +0530

    setup project with msg

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -160,6 +160,7 @@ await step.run("save-result ", async () => {
   if(isError){
      return await prisma.message.create({
       data: {
+        projectId: event.data.projectId,
         content: "Something went wrong. Please try again.",
         role: "ASSISTANT",
         type: "ERROR",
@@ -169,6 +170,7 @@ await step.run("save-result ", async () => {
 
   return await prisma.message.create({
     data: {
+      projectId: event.data.projectId,
       content: result.state.data.summary,
       role: "ASSISTANT",
       type: "RESULT",
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,3 +1,4 @@
+import { projectsRouter } from '@/modules/projects/procedures';
 import { createTRPCRouter } from '../init';
 import { messagesRouter } from '@/modules/messages/procedures';
 
@@ -5,6 +6,7 @@ import { messagesRouter } from '@/modules/messages/procedures';
 
 export const appRouter = createTRPCRouter({
    messages: messagesRouter,
+   projects: projectsRouter,
 })
   
 // export type definition of API
